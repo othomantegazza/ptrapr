a7e8949c363f60338712157313c25b35c1a5cd9e
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-12-03 11:22:01 +0100

    Document data

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' A Panicle Graph
+#'
+#' A Graph Storing Vertexes and Edges for A rice Panicle
+#'
+#' @format `IGRAPH` with 129 vertexes and 128 edges
+#' @source Produced by Helene Adam at IRD.
+
+"good_panicle"
+
+#' A Panicle Graph with Inverted Edges
+#'
+#' A Graph Storing Vertexes and Edges for A rice Panicle.
+#' The direction of the edges on the main axis (rachis) is
+#' inverted.
+#'
+#' This dataset can be used to illustrate how to invert
+#' the directions of the edges on the main axis with
+#' `invert_edges()`.
+#'
+#' @format `IGRAPH` with 134 vertexes and 133 edges
+#' @source Produced by Helene Adam at IRD.
+
+"inverted_panicle"
+
+"good_panicle"
