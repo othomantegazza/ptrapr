374ce36246ba17ac2de5c1628e26f7c8b25429a2
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-12-06 13:46:16 +0100

    Parse both panicle files with a single argument

--- R/wrapper-parse-and-merge.R
+++ R/wrapper-parse-and-merge.R
@@ -5,9 +5,20 @@
 #' file (which stores the structure of a panicle).
 #' - A a `.ricegr`
 #' file (which stores the coordinates of grains).
-#'
+
 #' And merges them together
 #'
+#' If the name of the `.ricepr` and the `.ricegr` is the same,
+#' you can provide this name without extension to the parameter
+#' `base_path`. Otherwise, you can provide the path to the two
+#' files with their extensions to the parameters
+#'`pr_file` and `gr_file` .
+#'
+#' @param base_path The path to the panicle files without extension.
+#'     Use this parameter only if the names of the panicle structure file
+#'     and of the panicle structure files are the same, and they are in the
+#'     same folder. The `base_path` parameter and the `pr_file` + `gr_file`
+#'     parameters are mutually exclusive. Use the first, or the others.
 #' @param pr_file The path to the `.ricepr` file,
 #'  (which stores the structure of a panicle)
 #' @param gr_file The path to the `.ricegr` file,
@@ -15,9 +26,14 @@
 #'
 #' @export
 
-read_full_panicle <- function(pr_file,
+read_full_panicle <- function(base_path = NULL,
+                              pr_file,
                               gr_file)
 {
+  if(!is.null(base_path)) {
+    pr_file <- paste0(base_path, ".ricepr")
+    gr_file <- paste0(base_path, ".ricegr")
+  }
   # Read Panicle structure
   panicle <-
     read_panicle(path = pr_file)
