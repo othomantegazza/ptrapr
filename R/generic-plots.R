fc2431d8428f2939143b4ff4f4b5bcbbd095420b
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-12-19 16:25:01 +0100

    Now points and eddges in graphs do not overlap

--- R/generic-plots.R
+++ R/generic-plots.R
@@ -5,23 +5,29 @@
 #' `ggraph()` directly.
 #'
 #' @param panicle_graph a panicle graph
+#' @param point_size Numeric, the size of the points in the graph. You need
+#'     to set this parameter if you want that the edges and the points in the
+#'     graph stop overlapping. Defaults to 2 (mm).
 #'
 #' @export
 
-plot_panicle <- function(panicle_graph)
+plot_panicle <- function(panicle_graph,
+                         point_size = 1.8)
 {
   p <-
     panicle_graph %>%
     ggraph::ggraph(layout = "nicely") +
     ggraph::geom_edge_link(
       arrow = grid::arrow(
-        length = grid::unit(0.08, "inches"),
+        length = grid::unit(0.06, "inches"),
         type = "closed"),
-      colour = "grey30") +
+      colour = "grey30",
+      end_cap = ggraph::circle(point_size/2, "mm"),
+      start_cap = ggraph::circle(point_size/2, "mm")) +
     ggraph::geom_node_point(
       ggplot2::aes_string(colour = "type"),
-      size = 2,
-      alpha =.7) +
+      size = point_size,
+      alpha =.9) +
     ggplot2::coord_fixed() +
     ggplot2::theme_minimal()
 
